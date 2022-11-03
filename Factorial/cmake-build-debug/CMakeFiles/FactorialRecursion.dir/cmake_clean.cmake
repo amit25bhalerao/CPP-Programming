7886a41ca228255a846e3ac9538c74b50abe12c9
amit25bhalerao <115334346+amit25bhalerao@users.noreply.github.com>
2022-11-03 08:51:28 +0530

    Add files via upload

---
+++ Factorial/cmake-build-debug/CMakeFiles/FactorialRecursion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FactorialRecursion.dir/main.cpp.obj"
+  "FactorialRecursion.exe"
+  "FactorialRecursion.exe.manifest"
+  "FactorialRecursion.pdb"
+  "libFactorialRecursion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FactorialRecursion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
